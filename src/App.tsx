e2e3a5a9491317f8070930bd1d234dcf8fcccd38
asamaga <57379492+asamaga18@users.noreply.github.com>
2025-04-12 02:51:43 -0400

    changed to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './auth/Login';
 import Signup from './auth/Signup';
 import Post from './Post/Post';
