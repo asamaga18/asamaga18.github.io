c927a1d75d500dd9be65ec668879ca698923795d
asamaga <57379492+asamaga18@users.noreply.github.com>
2025-04-12 04:07:02 -0400

    testing new links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,27 +6,27 @@ const Sidebar = () => {
 
   return (
     <div className="sidebar">
-      <Link to="/thetomatotrade/post" className="plus-btn">
+      <Link to="/post" className="plus-btn">
         <i className="fas fa-plus"></i>
       </Link>
       
       <nav className="menu">
-        <Link to="/thetomatotrade/home" className={`menu-item ${location.pathname === '/thetomatotrade/home' ? 'active' : ''}`}>
+        <Link to="/home" className={`menu-item ${location.pathname === '/home' ? 'active' : ''}`}>
           <i className="fas fa-home"></i>
           <span>Home</span>
         </Link>
         
-        <Link to="/thetomatotrade/browse" className={`menu-item ${location.pathname === '/thetomatotrade/browse' ? 'active' : ''}`}>
+        <Link to="/browse" className={`menu-item ${location.pathname === '/browse' ? 'active' : ''}`}>
           <i className="fas fa-search"></i>
           <span>Browse</span>
         </Link>
         
-        <Link to="/thetomatotrade/messages" className={`menu-item ${location.pathname === '/thetomatotrade/messages' ? 'active' : ''}`}>
+        <Link to="/messages" className={`menu-item ${location.pathname === '/messages' ? 'active' : ''}`}>
           <i className="fas fa-comments"></i>
           <span>Messages</span>
         </Link>
         
-        <Link to="/thetomatotrade/profile" className={`menu-item ${location.pathname === '/thetomatotrade/profile' ? 'active' : ''}`}>
+        <Link to="/profile" className={`menu-item ${location.pathname === '/profile' ? 'active' : ''}`}>
           <i className="fas fa-user"></i>
           <span>Profile</span>
         </Link>
