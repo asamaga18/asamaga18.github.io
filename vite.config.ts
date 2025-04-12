52244163ed723f6df0f273781f68b39e66462051
asamaga <57379492+asamaga18@users.noreply.github.com>
2025-04-12 11:43:10 -0400

    Revert "testing new stuff"
    This reverts commit 6d43e6d16582c5ca7db55a731bd2593cd4a42141.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/thetomatotrade/', // 👈 this is crucial for GitHub Pages
   plugins: [react()],
 })
 
