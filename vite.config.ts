13197b862410426653372887d09328f1d611c15e
asamaga <57379492+asamaga18@users.noreply.github.com>
2025-04-12 02:54:43 -0400

    added some code for ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/thetomatotrade/', // 👈 this is crucial for GitHub Pages
   plugins: [react()],
 })
+
