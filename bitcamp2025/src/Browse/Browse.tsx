8a4106425439345f29b636028d8b9ad03c76e98f
Mags-09 <mcandmag@terpmail.umd.edu>
2025-04-12 04:21:18 +0000

    copied over the twitter clone mapping info for Browse page

--- bitcamp2025/src/Browse/Browse.tsx
+++ bitcamp2025/src/Browse/Browse.tsx
@@ -1,5 +1,30 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
-import Select from 'react-select';
 import './Browse.css';
 import './Browse';
+
+// App.js
+import Post from './src/Post';
+import { postData } from './src/postData';
+
+/* Do not write your tweet component here, write it in the tweet.tsx file */
+
+function Browse() {
+  return (
+    <div className="app">
+      <h1>Twitter Clone</h1>
+      <div className="feed">
+        {postData.map(Post => (
+          <Post
+            key = {Post.id}
+            sender = {Post.username}
+            message = {Post.content}
+            startingLikes = {Post.likes}
+            timeStamp = {Post.timestamp}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Browse;
