caa02675d7d4c540250e40b31f2511cda907d54f
asamaga <57379492+asamaga18@users.noreply.github.com>
2025-04-13 00:11:49 -0400

    changed procfile stuff

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app 
+web: gunicorn backend.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
+# from backend.main import app
+
+# if __name__ == "__main__":
+#     app.run() 
+
 from backend.main import app
 
 if __name__ == "__main__":
-    app.run() 
+    import uvicorn
+    uvicorn.run(app)
